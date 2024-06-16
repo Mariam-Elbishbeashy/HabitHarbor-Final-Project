624c0ca713ad1a54e18f63fdd27d70ef6b0e7556
Mariam Elbishbeashy <mariam.elbishbeashy@gmail.com>
2024-06-17 02:07:24 +0300

    added resources and posts.ejs also modify file constructure and app.js

---
+++ HabitHarbor/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const path = require('path');
+
+// express app
+const app = express();
+const port = 3000;
+const dbURI = ''
+// mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(result => app.listen(8080))
+//   .catch(err => console.log(err));
+
+ app.use(express.urlencoded({ extended: true }));
+
+// // default options
+ app.use(fileUpload());
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+
+// app.use("/", indexRoutes);
+// app.use("/user", userRoutes);
+// app.use("/admin", adminRoutes);
+
+
+app.get('/posts', (req, res) => {
+  res.render('posts');
+});
+
+//404 page
+// app.use((req, res) => {
+//   res.status(404).render('404', { user: (req.session.user === undefined ? "" : req.session.user) });
+// });
+app.listen(port, () =>{
+  console.log("app listening on port 3000");
+});
