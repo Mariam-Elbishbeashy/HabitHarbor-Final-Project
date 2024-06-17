1b57117078dd33377ec31dd93902c5eb9f59c7a7
Mariam Elbishbeashy <mariam.elbishbeashy@gmail.com>
2024-06-17 23:04:37 +0300

    created resources mongodb also edited and fixed some problems in paths

--- HabitHarbor/app.js
+++ HabitHarbor/app.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const mongoose = require('mongoose');
 const path = require('path');
+const Resource = require('./models/resourcedb');
+const data = require('./models/resourcedata');
 
 // express app
 const app = express();
 const port = 3000;
-const dbURI = ''
-// mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(result => app.listen(8080))
-//   .catch(err => console.log(err));
+const dbURI = 'mongodb://localhost:27017/HabitHarborDB';
 
- app.use(express.urlencoded({ extended: true }));
+// Connect to MongoDB
+mongoose.connect(dbURI)
+  .then(async () => {
+    await Resource.deleteMany({}); 
+    const result = await Resource.insertMany(data);
+    console.log(`${result.length} documents inserted successfully`);
 
-// // default options
- app.use(fileUpload());
+    // Start express server after inserting data
+    app.listen(port, () => {
+      console.log(`App listening on port ${port}`);
+    });
+  })
+  .catch(err => console.log(err));
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
@@ -34,11 +47,12 @@ app.get('/admin', (req, res) => {
   res.render('admin');
 });
 
+app.get('/resources', (req, res) => {
+  res.render('resources');
+});
+
 
 //404 page
 // app.use((req, res) => {
 //   res.status(404).render('404', { user: (req.session.user === undefined ? "" : req.session.user) });
 // });
-app.listen(port, () =>{
-  console.log("app listening on port 3000");
-});
---
+++ HabitHarbor/models/resourcedata.js
@@ -0,0 +1,39 @@
+
+data =[{
+    Title: "12 Benefits of Employee Training: Why is Employee Training Important?.",
+    Image: "images/featured-2.png",
+    Paragraph: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusane dolores, possimus pariatur animi temporibus nesciunt praesentium ",
+    URL: "https://www.chrmp.com/benefits-of-employee-training/",
+    Category: "training"
+  },
+  {
+    Title: "6 Unexpected Ways to Create Good Habits",
+    Image: "images/getty_509392291_332681.jpg",
+    Paragraph: "The fastest way to achieve your dreams is to commit to them. Once you are committed to something, nothing else will stand in your way... ",
+    URL: "https://www.inc.com/mareo-mccracken/6-unexpected-ways-to-create-good-habits-and-actually-keep-them.html",
+    Category: "habits"
+  },
+  {
+    Title: "What Does It Really Take to Build a New Habit?",
+    Image: "images/Feb21_02_1285466367.jpg",
+    Paragraph: "Habit and routines are not interchangeable. A habit is a behavior done with little or no thought, whereas a routine is a series of behaviors frequently, and intentionally, repeated. To turn a behavior into a habit, it first needs to become a routine. How do you start? ",
+    URL: "https://hbr.org/2021/02/what-does-it-really-take-to-build-a-new-habit",
+    Category: "habits"
+  },
+  {
+    Title: "What Does It Really Take to Build a New Habit?",
+    Image: "images/six-tips-to-build-good-habits.png",
+    Paragraph: "Habits are the secret sauce to a healthy life - or the recipe for an unhealthy one, depending on what's in question. ",
+    URL: "https://blog.zencare.co/how-to-build-good-habits/",
+    Category: "habits"
+  },
+  {
+    Title: "How To Build A Habit? 5 Scientific-Backed Steps For Change",
+    Image: "images/how-to-build-a-habit.jpg",
+    Paragraph: "as derived from the book “How to Change.” These steps serve as a roadmap for individuals seeking to transition from where they are to where they aspire to be!",
+    URL: "https://themindsjournal.com/news/how-to-build-a-habit/",
+    Category: "habits"
+  }
+  ]
+
+  module.exports = data;
